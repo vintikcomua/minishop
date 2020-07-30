7003cca313de688d1cb7d6880a9bf6a4f8820b81
vintikcomua <vintikcomua@gmail.com>
2020-07-30 16:40:04 +0300

    new menu items

--- translations/minishop_uk_UA.ts
+++ translations/minishop_uk_UA.ts
@@ -17,29 +17,251 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../forms/main/mainwindow.ui" line="14"/>
+        <location filename="../forms/main/mainwindow.ui" line="20"/>
         <source>Mini Shop</source>
         <translation>Крамничка</translation>
     </message>
     <message>
-        <location filename="../forms/main/mainwindow.ui" line="32"/>
+        <location filename="../forms/main/mainwindow.ui" line="38"/>
         <source>File</source>
         <translation>Файл</translation>
     </message>
     <message>
-        <location filename="../forms/main/mainwindow.ui" line="39"/>
+        <location filename="../forms/main/mainwindow.ui" line="47"/>
         <source>Help</source>
         <translation>Довідка</translation>
     </message>
     <message>
         <location filename="../forms/main/mainwindow.ui" line="54"/>
+        <source>Edit</source>
+        <translation>Правка</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="69"/>
+        <source>Dictionary</source>
+        <translation>Довідники</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="73"/>
+        <source>Classifiers</source>
+        <translation>Класифікатори</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="90"/>
+        <source>toolBar</source>
+        <translation>панель інструментів</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="105"/>
         <source>Quit</source>
         <translation>Вийти</translation>
     </message>
     <message>
-        <location filename="../forms/main/mainwindow.ui" line="62"/>
+        <location filename="../forms/main/mainwindow.ui" line="108"/>
+        <source>Exit the application</source>
+        <translation>Вийти з додатка</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="117"/>
         <source>About</source>
         <translation>Про програму</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="120"/>
+        <source>Show the application&apos;s About box</source>
+        <translation type="unfinished">Показати вікно про програму</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="132"/>
+        <source>Cut</source>
+        <translation>Вирізати</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="135"/>
+        <source>Cut the current selection&apos;s contents to the clipboard</source>
+        <translation>Вирізати вміст поточного виділеного фрагмента в буфер обміну</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="138"/>
+        <source>Ctrl+X</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="150"/>
+        <source>Copy</source>
+        <translation>Копіювати</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="153"/>
+        <source>Copy the current selection&apos;s contents to the clipboard</source>
+        <translation>Скопіюйте вміст поточного вибору в буфер обміну</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="156"/>
+        <source>Ctrl+C</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="168"/>
+        <source>Paste</source>
+        <translation>Вставити</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="171"/>
+        <source>Paste the clipboard&apos;s contents into the current selection</source>
+        <translation>Вставте вміст буфера обміну в поточний вибір</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="174"/>
+        <source>Ctrl+V</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="186"/>
+        <source>Undo</source>
+        <translation>Повернути</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="189"/>
+        <source>Undo the last operation</source>
+        <translation>Скасувати останню операцію</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="192"/>
+        <source>Ctrl+Z</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="204"/>
+        <source>Redo</source>
+        <translation>Повторити</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="207"/>
+        <source>Redo the last operation</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="210"/>
+        <source>Ctrl+Y</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="222"/>
+        <source>Select All</source>
+        <translation>Виділити все</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="225"/>
+        <source>Select all text</source>
+        <translation>вибрати увесь текст</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="228"/>
+        <source>Ctrl+A</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="240"/>
+        <source>Find ...</source>
+        <translation>Знайти ...</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="243"/>
+        <source>Find text</source>
+        <translation>Пошик тексту</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="246"/>
+        <source>Ctrl+F</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="258"/>
+        <location filename="../forms/main/mainwindow.ui" line="261"/>
+        <location filename="../forms/main/mainwindow.ui" line="264"/>
+        <source>Find &amp;&amp; Replace ...</source>
+        <translation>Знайти та замінити ...</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="267"/>
+        <source>Finf &amp;&amp; replace text</source>
+        <translation>Пошук та заміна тексту</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="270"/>
+        <source>Ctrl+H</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="275"/>
+        <source>Nomenclature</source>
+        <translation>Номенклатура</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="278"/>
+        <source>Open dictionary Nomenclature</source>
+        <translation>Відкрити довідник Номенклатура</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="283"/>
+        <source>Manufacturers</source>
+        <translation>Виробники</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="286"/>
+        <source>Open dictionary Manufacturers</source>
+        <translation>Відкрити довідник Виробники</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="291"/>
+        <source>Units of measurement</source>
+        <translation>Одиниці виміру</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="294"/>
+        <source>Open dictionary Units of measurement</source>
+        <translation>Відкрити довідник Одиниці виміру</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="299"/>
+        <source>Counterparties</source>
+        <translation>Контрагенти</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="302"/>
+        <source>Open dictionary Counterparties</source>
+        <translation>Відкрити довідник Контрагентів</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="314"/>
+        <source>Print...</source>
+        <translation>Друк ...</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="317"/>
+        <source>Print the document</source>
+        <translation>Друк документа</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="320"/>
+        <source>Ctrl+P</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="332"/>
+        <source>Print Preview</source>
+        <translation>Перегляд друку</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="335"/>
+        <source>Print preview the document</source>
+        <translation>Перегляд друку документа</translation>
+    </message>
+    <message>
+        <location filename="../forms/main/mainwindow.ui" line="338"/>
+        <source>Ctrl+Shift+P</source>
+        <translation></translation>
     </message>
 </context>
 </TS>
